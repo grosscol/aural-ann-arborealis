5eab06d45cd396e31faee92118a8b5d19fc75a74
Colin Gross <grosscol@gmail.com>
2019-01-14 10:52:21 -0500

    Add bookend case for counting conflicts between vectors.

--- tests/testthat/test_count_conflicting.R
+++ tests/testthat/test_count_conflicting.R
@@ -2,3 +2,12 @@ test_that('returns 0 when given empty vectors', {
   result <- count_conflicting(c(), c())
   expect_equal(result, 0)
 })
+
+test_that('returns length of args when no elements line up', {
+  len <- 10
+  lhs <- seq(1:len)
+  rhs <- rev(lhs)
+
+  result <- count_conflicting(lhs, rhs)
+  expect_equal(result, len)
+})
