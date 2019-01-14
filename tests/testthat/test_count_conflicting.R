aaaa6aa83a5275b9b34413e6d663072ce85ee9a6
Matthew Alexander LaChance <mattlach@umich.edu>
2019-01-14 11:03:28 -0500

    count_conflicting() returns 0 for equal lists

--- tests/testthat/test_count_conflicting.R
+++ tests/testthat/test_count_conflicting.R
@@ -11,3 +11,12 @@ test_that('returns length of args when no elements line up', {
   result <- count_conflicting(lhs, rhs)
   expect_equal(result, len)
 })
+
+test_that('returns length of 0 when all elements line up', {
+  len <- 10
+  lhs <- seq(1:len)
+  rhs <- lhs
+
+  result <- count_conflicting(lhs, rhs)
+  expect_equal(result, 0)
+})
